998b47ecf42d67a70506787e01e90887349ef639
cangjie <cangjie@hotmail.com>
2016-10-14 16:13:10 +0800

    semicon

--- App_Code/DealMessage.cs
+++ App_Code/DealMessage.cs
@@ -42,7 +42,7 @@ public class DealMessage
         repliedMessage.from = receivedMessage.to.Trim();
         repliedMessage.to = receivedMessage.from;
         repliedMessage.type = "text";
-        repliedMessage.content = "感谢您关注易龙雪聚，请<a href='http://weixin.snowmeet.com/pages/register_cell_number.aspx' >点击这里</a>以完成注册。";
+        repliedMessage.content = "感谢您关注易龙雪聚，请<a href=\"http://weixin.snowmeet.com/pages/register_cell_number.aspx\" >点击这里</a>以完成注册。";
         return repliedMessage;
     }
 
