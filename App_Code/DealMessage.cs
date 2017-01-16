bceee66985c8c612e4d4e75aa9ce898cd23cfb5a
cangjie <cangjie@hotmail.com>
2017-01-16 16:03:06 +0800

    rebuild code

--- App_Code/DealMessage.cs
+++ App_Code/DealMessage.cs
@@ -108,165 +108,86 @@ public class DealMessage
         switch (receivedMessage.userEvent.ToUpper())
         {
             case "SCAN":
-                
-                if (receivedMessage.eventKey.Trim().StartsWith("3") && user.IsAdmin)
+                if (receivedMessage.eventKey.Length == 10)
                 {
-                    try
+                    if (receivedMessage.eventKey.Trim().StartsWith("3") && user.IsAdmin)
                     {
-                        string ticketCode = receivedMessage.eventKey.Substring(1, 9);
-                        Card card = new Card(ticketCode);
-                        repliedMessage.type = "news";
-                        RepliedMessage.news content = new RepliedMessage.news();
-                        if (card.Used)
+                        try
                         {
-                            repliedMessage.type = "text";
-                            repliedMessage.content = card._fields["type"].ToString() + ":"
-                                + ticketCode.Trim() + "已经使用，点击<a href=\"http://weixin.snowmeet.com/pages/admin/wechat/card_confirm_finish.aspx?code=" + ticketCode.Trim()  +"\" >这里</a>查看详情";
-
-                        }
-                        else
-                        {
-                            if (card._fields["type"].ToString().Equals("雪票"))
+                            string ticketCode = receivedMessage.eventKey.Substring(1, 9);
+                            Card card = new Card(ticketCode);
+                            repliedMessage.type = "news";
+                            RepliedMessage.news content = new RepliedMessage.news();
+                            if (card.Used)
                             {
-                                content.title = "确认雪票-" + ticketCode;
-                                content.picUrl = "http://www.nanshanski.com/web_cn/images/bppt.jpg";
-                                content.url = "http://weixin.snowmeet.com/pages/admin/wechat/card_confirm.aspx?code=" + ticketCode.Trim();
-                                content.description = "";
-                            }
-                            else
-                            {
-                                content.title = "确认消费抵用券-" + ticketCode;
-                                content.picUrl = "http://www.nanshanski.com/web_cn/images/bppt.jpg";
-                                content.url = "http://weixin.snowmeet.com/pages/admin/wechat/ticket_confirm.aspx?code=" + ticketCode.Trim();
-                                content.description = "";
-                            }
-
-                            repliedMessage.newsContent = new RepliedMessage.news[] { content };
-                            
-                        }
-                        return repliedMessage;
-                    }
-                    catch
-                    {
-
-                    }
-                }
-
-                //签到
-                double latNorth = 0;
-                double latSourth = 0;
-                double lonWest = 0;
-                double lonEast = 0;
-
-                
-
-                Location lastLocation;
-                switch (receivedMessage.eventKey)
-                {
-                    case "1":
-                        Thread.Sleep(3000);
-                        string nanShanLocResult = Location.FindInResort(receivedMessage.from);
-                        if (nanShanLocResult.Trim().Equals("南山"))
-                        {
-                            repliedMessage.content = "欢迎签到易龙雪聚南山店。";
-
-                            if (user.VipLevel < 1)
-                            {
-                                repliedMessage.content = repliedMessage.content + "请<a href=\"http://weixin.snowmeet.com/pages/register_cell_number.aspx\" >点击这里</a>绑定手机号码以获得签到积分。";
-                            }
-                            else
-                            {
-                                Point.AddNew(user.OpenId.Trim(), 10, DateTime.Now, "南山签到");
-                            }
-
-                        }
-                        else
-                        {
-                            if (!nanShanLocResult.Trim().Equals(""))
-                                repliedMessage.content = nanShanLocResult.Trim();
-                            else
-                                repliedMessage.content = "您不在南山，不能签到。";
-                        }
-                        repliedMessage.type = "text";
-                        return repliedMessage;
-                        break;
-                    case "2":
-                        Thread.Sleep(2500);
-                        string baYiLocResult = Location.FindInResort(receivedMessage.from);
-                        if (baYiLocResult.Trim().Equals("八易"))
-                        {
-                            repliedMessage.content = "欢迎签到易龙雪聚八易店。";
-                            if (user.VipLevel < 1)
-                            {
-                                repliedMessage.content = repliedMessage.content + "请<a href=\"http://weixin.snowmeet.com/pages/register_cell_number.aspx\" >点击这里</a>绑定手机号码以获得签到积分。";
-                            }
-                            else
-                            {
-                                Point.AddNew(user.OpenId.Trim(), 10, DateTime.Now, "八易签到");
-                            }
-                        }
-                        else
-                        {
-                            if (!baYiLocResult.Trim().Equals(""))
-                                repliedMessage.content = baYiLocResult.Trim();
-                            else
-                                repliedMessage.content = "您不在八易，不能签到。";
-                        }
-                        repliedMessage.type = "text";
-                        return repliedMessage;
-                        break;
-                    default:
-                        break;
-                }
-
-
-                if (receivedMessage.eventKey.StartsWith("1"))
-                {
-                    if (user.VipLevel == 0)
-                    {
-                        repliedMessage.content =  "请<a href=\"http://weixin.snowmeet.com/pages/register_cell_number.aspx\" >点击这里</a>绑定手机号码后重新扫描设备的二维码。";
-                    }
-                    else
-                    {
-                        Device device = Device.ScanDeviceQrCode(receivedMessage.eventKey);
-                        if (device != null)
-                        {
-                            int needPoints = int.Parse(device._fields["need_point"].ToString());
-                            if (user.Points < needPoints)
-                                Device.FirstTimeToUse(receivedMessage.from.Trim(), int.Parse(device._fields["id"].ToString()));
-                            if (user.Points >= needPoints)
-                            {
-                                Point.AddNew(user.OpenId, -1 * needPoints, DateTime.Now, "使用" + device._fields["name"].ToString());
-                                Device.AddNewScanRecord(int.Parse(device._fields["id"].ToString()), Util.GetTimeStamp(), user.OpenId);
                                 repliedMessage.type = "text";
-                                repliedMessage.content = "请稍候，设备将在5秒钟内启动。";
+                                repliedMessage.content = card._fields["type"].ToString() + ":"
+                                    + ticketCode.Trim() + "已经使用，点击<a href=\"http://weixin.snowmeet.com/pages/admin/wechat/card_confirm_finish.aspx?code=" + ticketCode.Trim() + "\" >这里</a>查看详情";
 
                             }
                             else
                             {
-                                /*
-                                ServiceMessage userMessage = new ServiceMessage();
-                                userMessage.from = "gh_0427e9838339";
-                                userMessage.to = user.OpenId;
-                                userMessage.type = "text";
-                                userMessage.content = "您的龙珠不够。";
-                                ServiceMessage.SendServiceMessage(userMessage);
-                                 */
-                                
-                                repliedMessage.content = "您的龙珠不够。";
+                                if (card._fields["type"].ToString().Equals("雪票"))
+                                {
+                                    content.title = "确认雪票-" + ticketCode;
+                                    content.picUrl = "http://www.nanshanski.com/web_cn/images/bppt.jpg";
+                                    content.url = "http://weixin.snowmeet.com/pages/admin/wechat/card_confirm.aspx?code=" + ticketCode.Trim();
+                                    content.description = "";
+                                }
+                                else
+                                {
+                                    content.title = "确认消费抵用券-" + ticketCode;
+                                    content.picUrl = "http://www.nanshanski.com/web_cn/images/bppt.jpg";
+                                    content.url = "http://weixin.snowmeet.com/pages/admin/wechat/ticket_confirm.aspx?code=" + ticketCode.Trim();
+                                    content.description = "";
+                                }
+
+                                repliedMessage.newsContent = new RepliedMessage.news[] { content };
 
                             }
-
+                            return repliedMessage;
+                        }
+                        catch
+                        {
 
                         }
                     }
-                    repliedMessage.type = "text";
+
+                    if (int.Parse(receivedMessage.eventKey.Substring(0,2))>13)
+                    {
+                        repliedMessage = ScanDeviceQrCode(receivedMessage);
+                    }
+
+                    if (receivedMessage.eventKey.Trim().StartsWith("4294"))
+                    {
+                        
+                    }
+
                 }
+                else
+                {
+                    if (receivedMessage.eventKey.Trim().Equals("1") || receivedMessage.eventKey.Equals("2"))
+                    {
+                        repliedMessage = ScanSignin(receivedMessage);
+                    }
+                }
+
                 break;
             case "subscribe":
-                if (receivedMessage.eventKey.StartsWith("qrscene_1"))
+                string eventKey = receivedMessage.eventKey.Replace("qrscene_", "").Trim();
+                if (eventKey.Length == 10)
                 {
-
+                    if (int.Parse(eventKey.Substring(0, 2)) > 13)
+                    {
+                        repliedMessage = ScanDeviceQrCode(receivedMessage);
+                    }
+                }
+                else
+                {
+                    if (eventKey.Trim().Equals("1") || eventKey.Equals("2"))
+                    {
+                        repliedMessage = ScanSignin(receivedMessage);
+                    }
                 }
                 break;
             default:
@@ -300,19 +221,114 @@ public class DealMessage
 
     public static RepliedMessage CreateQrCodeReplyMessage(ReceivedMessage receivedMessage, RepliedMessage repliedMessage)
     {
-        /*
-        string token = Util.GetToken();
-        long scene = long.Parse(Util.GetInviteCode(receivedMessage.to.Trim()));
-        string ticket = Util.GetQrCodeTicketTemp(token, scene);
-        byte[] qrCodeByteArr = Util.GetQrCodeByTicket(ticket);
-        string filePathName = System.Configuration.ConfigurationSettings.AppSettings["qrcode_path"].Trim() + "\\" + scene.ToString() + ".jpg";
-        Util.SaveBytesToFile(filePathName, qrCodeByteArr);
-        string mediaId = Util.UploadImageToWeixin(filePathName, token);
-        */
         WeixinUser user = new WeixinUser(receivedMessage.from);
         repliedMessage.messageCount = 1;
         repliedMessage.type = "text";
         repliedMessage.content = "<a href=\"http://weixin.snowmeet.com/show_qrcode.aspx?sceneid=" + user.QrCodeSceneId.ToString() + "\"  >点击查看二维码</a>";
+        return repliedMessage;
+    }
+
+    public static RepliedMessage ScanDeviceQrCode(ReceivedMessage receivedMessage)
+    {
+        RepliedMessage repliedMessage = new RepliedMessage();
+        repliedMessage.from = receivedMessage.to;
+        repliedMessage.to = receivedMessage.from;
+        string eventKey = receivedMessage.eventKey.Replace("qrcode_", "");
+        WeixinUser user = new WeixinUser(receivedMessage.from.Trim());
+        if (user.VipLevel == 0)
+        {
+            repliedMessage.content = "请<a href=\"http://weixin.snowmeet.com/pages/register_cell_number.aspx\" >点击这里</a>绑定手机号码后重新扫描设备的二维码。";
+        }
+        else
+        {
+            Device device = Device.ScanDeviceQrCode(eventKey);
+            if (device != null)
+            {
+                int needPoints = int.Parse(device._fields["need_point"].ToString());
+                if (user.Points < needPoints)
+                    Device.FirstTimeToUse(receivedMessage.from.Trim(), int.Parse(device._fields["id"].ToString()));
+                if (user.Points >= needPoints)
+                {
+                    Point.AddNew(user.OpenId, -1 * needPoints, DateTime.Now, "使用" + device._fields["name"].ToString());
+                    Device.AddNewScanRecord(int.Parse(device._fields["id"].ToString()), Util.GetTimeStamp(), user.OpenId);
+                    repliedMessage.type = "text";
+                    repliedMessage.content = "请稍候，设备将在5秒钟内启动。";
+
+                }
+                else
+                {
+
+                    repliedMessage.content = "您的龙珠不够。";
+                }
+            }
+        }
+        repliedMessage.type = "text";
+        return repliedMessage;
+    }
+
+    public static RepliedMessage ScanSignin(ReceivedMessage receivedMessage)
+    {
+        RepliedMessage repliedMessage = new RepliedMessage();
+        repliedMessage.from = receivedMessage.to;
+        repliedMessage.to = receivedMessage.from;
+        string eventKey = receivedMessage.eventKey.Replace("qrcode_", "");
+        WeixinUser user = new WeixinUser(receivedMessage.from.Trim());
+        switch (eventKey)
+        {
+            case "1":
+                Thread.Sleep(3000);
+                string nanShanLocResult = Location.FindInResort(receivedMessage.from);
+                if (nanShanLocResult.Trim().Equals("南山"))
+                {
+                    repliedMessage.content = "欢迎签到易龙雪聚南山店。";
+
+                    if (user.VipLevel < 1)
+                    {
+                        repliedMessage.content = repliedMessage.content + "请<a href=\"http://weixin.snowmeet.com/pages/register_cell_number.aspx\" >点击这里</a>绑定手机号码以获得签到积分。";
+                    }
+                    else
+                    {
+                        Point.AddNew(user.OpenId.Trim(), 10, DateTime.Now, "南山签到");
+                    }
+
+                }
+                else
+                {
+                    if (!nanShanLocResult.Trim().Equals(""))
+                        repliedMessage.content = nanShanLocResult.Trim();
+                    else
+                        repliedMessage.content = "您不在南山，不能签到。";
+                }
+                repliedMessage.type = "text";
+                break;
+            case "2":
+                Thread.Sleep(2500);
+                string baYiLocResult = Location.FindInResort(receivedMessage.from);
+                if (baYiLocResult.Trim().Equals("八易"))
+                {
+                    repliedMessage.content = "欢迎签到易龙雪聚八易店。";
+                    if (user.VipLevel < 1)
+                    {
+                        repliedMessage.content = repliedMessage.content + "请<a href=\"http://weixin.snowmeet.com/pages/register_cell_number.aspx\" >点击这里</a>绑定手机号码以获得签到积分。";
+                    }
+                    else
+                    {
+                        Point.AddNew(user.OpenId.Trim(), 10, DateTime.Now, "八易签到");
+                    }
+                }
+                else
+                {
+                    if (!baYiLocResult.Trim().Equals(""))
+                        repliedMessage.content = baYiLocResult.Trim();
+                    else
+                        repliedMessage.content = "您不在八易，不能签到。";
+                }
+                repliedMessage.type = "text";
+                break;
+            default:
+                break;
+        }
+
         return repliedMessage;
     }
 
