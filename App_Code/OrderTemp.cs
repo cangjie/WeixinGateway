f73f05673c0b68e9b54882e1b992edaa07e01cd3
cangjie <cangjie@hotmail.com>
2017-01-22 23:24:35 +0800

    pay state

--- App_Code/OrderTemp.cs
+++ App_Code/OrderTemp.cs
@@ -38,10 +38,10 @@ public class OrderTemp
         WeixinUser user = new WeixinUser(openId);
         string[,] insertParam = { {"type", "varchar", "店销" }, { "open_id", "varchar", openId.Trim() },
         {"cell_number", "varchar", user.CellNumber.Trim() }, {"name", "varchar", user.Nick.Trim() }, 
-        {"pay_method", "varchar", "haojin" },{ "pay_state", "int", "0" },
+        {"pay_method", "varchar", "haojin" },{ "pay_state", "int", "1" },
         {"order_price", "float", _fields["market_price"].ToString() }, 
         {"order_real_pay_price", "float", _fields["real_paid_price"].ToString() }, {"memo", "varchar", _fields["memo"].ToString().Trim() },
-        {"pay_state", "int", "1" }, {"pay_time", "datetime", DateTime.Now.ToString() } };
+        {"pay_time", "datetime", DateTime.Now.ToString() } };
         int i = DBHelper.InsertData("order_online", insertParam);
         if (i == 1)
         {
