060dd341d3d15865f7d23e88c07466ab72507ae9
cangjie <cangjie@hotmail.com>
2018-12-27 20:24:30 +0800

    web.config

--- App_Code/DealMessage.cs
+++ App_Code/DealMessage.cs
@@ -251,7 +251,7 @@ public class DealMessage
                         repliedMessage.content = "请找" + assistant.Nick.Trim() + "在手机上完成操作";
                         SendCustomeRequestToAssistant(receivedMessage);
                     }
-                    /*
+                    
                     if (receivedMessage.eventKey.Trim().StartsWith("openid"))
                     {
                         string[] commandArr = receivedMessage.eventKey.Split('_');
@@ -266,7 +266,7 @@ public class DealMessage
                             }
                         }
                     }
-                    */
+                    
                     
                 }
 
