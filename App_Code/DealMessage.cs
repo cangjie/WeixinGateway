044a39be73f76f9770c2893cc5e672e84ddccb56
cangjiie <cangjie@hotmail.com>
2021-09-27 16:55:08 +0800

    p

--- App_Code/DealMessage.cs
+++ App_Code/DealMessage.cs
@@ -594,9 +594,13 @@ public class DealMessage
         repliedMessage.content = "您即将支付：" + order._fields["order_real_pay_price"].ToString()
             + "元。<a href=\"http://" + Util.domainName.Trim() + "/payment/payment.aspx?product_id=" + orderId.ToString() + "\" >点击此处支付</a>";
         */
+        /*
         repliedMessage.content = "您即将支付：" + order._fields["order_real_pay_price"].ToString()
             + "元。<p><a  data-miniprogram-appid=\"wxd1310896f2aa68bb\" data-miniprogram-path=\"pages/payment/payment?orderid=" 
             + orderId.ToString() + "\" href=\"\" >点击此处支付</a></p>";
+        */
+        repliedMessage.content = "<p><a  data-miniprogram-appid=\"wxd1310896f2aa68bb\" data-miniprogram-path=\"pages/payment/payment?orderid="
+                    + orderId.ToString() + "\" href=\"\" >点击此处支付</a></p>";
         return repliedMessage;
     }
 
